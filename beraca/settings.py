aa6b64aa26cfbfd1c2da2e8d7d182f7fa2bb22f4
samnklcf <samyeyebe@gmail.com>
2022-12-04 15:13:29 +0100

    samuel

--- beraca/settings.py
+++ beraca/settings.py
@@ -128,6 +128,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
                           
 TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'templates')]
